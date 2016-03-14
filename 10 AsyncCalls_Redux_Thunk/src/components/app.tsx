f5b170b848831707af3389faf4352f3843c5a498
nasdan <daniel.sanchez@lemoncode.net>
2016-03-14 12:16:57 +0100

    Fix reduxThunk typescript import

--- 10 AsyncCalls_Redux_Thunk/src/components/app.tsx
+++ 10 AsyncCalls_Redux_Thunk/src/components/app.tsx
@@ -3,25 +3,15 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import Header from './common/header'
 import reducers  from '../reducers';
-import * as reduxThunk from 'redux-thunk';
+import reduxThunk from 'redux-thunk';
 
 interface Props extends React.Props<App> {
 }
 
-// TODO: Hack, seems that tsd is out of date
-// Check on this threads which approach to follow
-// fix manually type definition (chekc to ask for pr), Check
-// check other ways to import the module
-// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/6231
-//import * as thunk from 'redux-thunk';
-// https://github.com/gaearon/redux-thunk/issues/35
-// https://github.com/Microsoft/TypeScript/issues/5565
-//import thunkMiddleware = require("redux-thunk");
-
-var myReduxThunk : any = reduxThunk;
-let store = createStore(reducers
-                        ,applyMiddleware(myReduxThunk.default)
-            );
+let store = createStore(
+  reducers
+  ,applyMiddleware(reduxThunk)
+);
 
 export default class App extends React.Component<Props, {}> {
    public render() {
