d6603134740498fdc87f0b928e3cb5b1f98116e5
Braulio <braulio.diez@gmail.com>
2016-03-17 11:40:50 +0100

    Simple action test implemented

---
+++ 13 TestAction/src/actions/spec/assignMembers.spec.ts
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import * as deepFreeze from 'deep-freeze';
+import assignMembers from '../assignMembers';
+import MemberEntity from '../../api/memberEntity';
+
+describe('assignMembers', () => {
+  it('should return members action type: MEMBERS_ASSIGN and members: member Array', () => {
+      // Arrange
+      const membersInput = new Array<MemberEntity>();
+
+      let member1 = new MemberEntity();
+      let member2 = new MemberEntity();
+
+      member1.login = 'test1';
+      member2.login = 'test2';
+
+      membersInput.push(member1);
+      membersInput.push(member2);
+
+      // Act
+      let result = assignMembers(membersInput);
+      // Assert
+
+      expect(result.type).to.be.equal('MEMBERS_ASSIGN');
+      expect(result.members.length).to.be.equal(2);
+      expect(result.members[0].login).to.be.equal('test1');
+      expect(result.members[1].login).to.be.equal('test2');
+  });
+});
