af20563ac24b3a593534901a066d6c1d6c023b0b
nasdan <daniel.sanchez@lemoncode.net>
2016-03-18 20:49:49 +0100

    Remove dependency with react and redux

--- 13 TestAction/typings-manual/redux-mock-store/redux-mock-store.d.ts
+++ 13 TestAction/typings-manual/redux-mock-store/redux-mock-store.d.ts
@@ -3,13 +3,13 @@
 // Definitions by: Braulio Díez <https://github.com/brauliodiez/>>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-/// <reference path="../../typings/react/react.d.ts" />
-/// <reference path="../../typings/redux/redux.d.ts" />
-
 declare module "redux-mock-store" {
-    interface MockStore extends Redux.Store {
-      getActions: () => Array<any>;
-      clearActions: () => void;
+    interface MockStore {
+        getState(): any;
+        getActions(): Array<any>;
+        dispatch(action: any): any;
+        clearActions(): void;
+        subscribe(): any;
     }
 
     function configureStore(...args: any[]) : (...args: any[]) => MockStore;
