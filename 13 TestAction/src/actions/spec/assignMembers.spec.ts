9792e654501b86f7a153380ed6443a9f86520a33
nasdan <daniel.sanchez@lemoncode.net>
2016-03-19 12:45:16 +0100

    Give more verbosity to assignMembers spec

--- 13 TestAction/src/actions/spec/assignMembers.spec.ts
+++ 13 TestAction/src/actions/spec/assignMembers.spec.ts
@@ -3,21 +3,22 @@ import assignMembers from '../assignMembers';
 import MemberEntity from '../../api/memberEntity';
 
 describe('assignMembers', () => {
-  it('should return members action type: MEMBERS_ASSIGN and members: []', () => {
+  it('should return members action type: MEMBERS_ASSIGN and members: [] when passing an empty members Array', () => {
       // Arrange
-      const membersInput = new Array<MemberEntity>();
-      // Act
-      let result = assignMembers(membersInput);
-      // Assert
+      const members = new Array<MemberEntity>();
 
+      // Act
+      let result = assignMembers(members);
+
+      // Assert
       expect(result.type).to.be.equal('MEMBERS_ASSIGN');
       expect(result.members.length).to.be.equal(0);
   });
 
-
-  it('should return members action type: MEMBERS_ASSIGN and members: member Array', () => {
+  it('should return members action type: MEMBERS_ASSIGN and members: member Array with two items when passing ' +
+        'a member Array with two items', () => {
       // Arrange
-      const membersInput = new Array<MemberEntity>();
+      const members = new Array<MemberEntity>();
 
       let member1 = new MemberEntity();
       let member2 = new MemberEntity();
@@ -25,13 +26,13 @@ describe('assignMembers', () => {
       member1.login = 'test1';
       member2.login = 'test2';
 
-      membersInput.push(member1);
-      membersInput.push(member2);
+      members.push(member1);
+      members.push(member2);
 
       // Act
-      let result = assignMembers(membersInput);
-      // Assert
+      let result = assignMembers(members);
 
+      // Assert
       expect(result.type).to.be.equal('MEMBERS_ASSIGN');
       expect(result.members.length).to.be.equal(2);
       expect(result.members[0].login).to.be.equal('test1');
