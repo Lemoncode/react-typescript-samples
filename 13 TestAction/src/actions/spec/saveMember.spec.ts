a1680f33a540bc68d18b538370ab5ef8e9a92336
nasdan <daniel.sanchez@lemoncode.net>
2016-03-21 16:13:22 +0100

    Refactor saveMember unit tests

--- 13 TestAction/src/actions/spec/saveMember.spec.ts
+++ 13 TestAction/src/actions/spec/saveMember.spec.ts
@@ -7,58 +7,61 @@ import MemberFormValidator from '../../validations/memberFormValidator';
 
 
 describe('saveMember', () => {
-
-  it('should return MEMBER_SAVE and errors.isEntityValid == true', () => {
+  it('should return an action equals { type: "MEMBER_SAVE", errors { isEntityValid: true } } and calls to ' +
+    'validateMember and saveAuthor methods when passing valid member', () => {
       // Arrange
-      const memberInput = new MemberEntity();
-      const mockedValidationError = new MemberErrors();
+      let member = new MemberEntity();
+      let mockedValidationError = new MemberErrors();
       mockedValidationError.isEntityValid = true;
 
-      sinon.stub(MemberFormValidator, 'validateMember').returns(mockedValidationError);
-      sinon.stub(memberAPI, 'saveAuthor').returns(null);
+      let validateMemberMethodStub = sinon.stub(MemberFormValidator, 'validateMember');
+      validateMemberMethodStub.returns(mockedValidationError);
+
+      let saveAuthorMethodStub = sinon.stub(memberAPI, 'saveAuthor');
 
       // Act
-      let result = saveMember(memberInput);
+      let result = saveMember(member);
 
       // Assert
       expect(result.type).to.be.equal('MEMBER_SAVE');
       expect(result.errors.isEntityValid).to.be.true;
+      expect(validateMemberMethodStub.called).to.be.true;
+      expect(saveAuthorMethodStub.called).to.be.true;
+      expect(saveAuthorMethodStub.calledWith(member)).to.be.true;
 
       // Tear down spies
       // We could place this in afterEach but we don't want to
       // force all test to mock this
-      const nonTypedMemberFormValidateMember = <any>MemberFormValidator.validateMember;
-      nonTypedMemberFormValidateMember.restore();
-
-      const nonTypedMemberApiSaveMember = <any>memberAPI.saveAuthor;
-      nonTypedMemberApiSaveMember.restore();
-
+      validateMemberMethodStub.restore();
+      saveAuthorMethodStub.restore();
   });
 
-
-  it('should return MEMBER_SAVE and errors.isEntityValid == false', () => {
+  it('should return an action equals { type: "MEMBER_SAVE", errors { isEntityValid: false } } and calls to ' +
+    'validateMember method when passing invalid member', () => {
       // Arrange
-      const memberInput = new MemberEntity();
-      const mockedValidationError = new MemberErrors();
+      let member = new MemberEntity();
+      let mockedValidationError = new MemberErrors();
       mockedValidationError.isEntityValid = false;
 
-      sinon.stub(MemberFormValidator, 'validateMember').returns(mockedValidationError);
-      sinon.stub(memberAPI, 'saveAuthor').returns(null);
+      let validateMemberMethodStub = sinon.stub(MemberFormValidator, 'validateMember');
+      validateMemberMethodStub.returns(mockedValidationError);
+
+      let saveAuthorMethodStub = sinon.stub(memberAPI, 'saveAuthor');
 
       // Act
-      let result = saveMember(memberInput);
+      let result = saveMember(member);
 
       // Assert
       expect(result.type).to.be.equal('MEMBER_SAVE');
       expect(result.errors.isEntityValid).to.be.false;
+      expect(validateMemberMethodStub.called).to.be.true;
+      expect(saveAuthorMethodStub.called).to.be.false;
+      expect(saveAuthorMethodStub.calledWith(member)).to.be.false;
 
       // Tear down spies
       // We could place this in afterEach but we don't want to
       // force all test to mock this
-      const nonTypedMemberFormValidateMember = <any>MemberFormValidator.validateMember;
-      nonTypedMemberFormValidateMember.restore();
-
-      const nonTypedMemberApiSaveMember = <any>memberAPI.saveAuthor;
-      nonTypedMemberApiSaveMember.restore();
+      validateMemberMethodStub.restore();
+      saveAuthorMethodStub.restore();
   });
 });
