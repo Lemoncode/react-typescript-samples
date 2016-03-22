d7940b55db83f497b0eed3a45d3171417e3ae926
nasdan <daniel.sanchez@lemoncode.net>
2016-03-22 13:07:32 +0100

    Added aboutPage unit test

--- 14 TestComponent/src/components/presentationals/about/spec/aboutPage.spec.tsx
+++ 14 TestComponent/src/components/presentationals/about/spec/aboutPage.spec.tsx
@@ -1,17 +1,22 @@
 import { expect } from 'chai';
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import AboutPage from '../aboutPage';
 
 describe('AboutPage presentational component', () =>{
-  it('should renders a h2 element with text equals "About Page" and a Link element with text equals "Members"', () => {
-    //Arrange
+    it('should renders a h2 element with text equals "About Page"', () => {
+        let aboutPageWrapper = shallow(<AboutPage />);
 
-    //Act
-    let AboutPageWrapper = shallow(<AboutPage />);
+        expect(aboutPageWrapper.find('h2')).to.have.length(1);
+        expect(aboutPageWrapper.find('h2').text()).to.be.equals("About Page");
+    });
 
-    //Assert
-    expect(AboutPageWrapper.find('h2')).to.have.length(1);
-    expect(AboutPageWrapper.find('h2').text()).to.be.equals("About Page");
-  })
+    it('should renders a Link element with text equals "Members" and property to equals "/members"', () => {
+        let aboutPageWrapper = shallow(<AboutPage />);
+
+        expect(aboutPageWrapper.find('Link')).to.have.length(1);
+        //Need access to children because is not basic HTML Element
+        expect(aboutPageWrapper.find('Link').children().text()).to.be.equals("Members");
+        expect(aboutPageWrapper.find('Link').prop('to')).to.be.equals("/members");
+    });
 })
