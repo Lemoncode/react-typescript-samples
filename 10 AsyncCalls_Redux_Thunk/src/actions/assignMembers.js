d5ea7bd12e0056eeb644eb992562baf8287d9a3d
algil <antonio.luis.gil@gmail.com>
2016-03-23 10:49:06 +0100

    Delete and ignore .js and .js.map files in src folder

--- 10 AsyncCalls_Redux_Thunk/src/actions/assignMembers.js
+++
@@ -1,9 +0,0 @@
-var assignMembers = function (members) {
-    return {
-        type: 'MEMBERS_ASSIGN',
-        members: members
-    };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = assignMembers;
-//# sourceMappingURL=assignMembers.js.map
--- 10 AsyncCalls_Redux_Thunk/src/actions/initializeNewMember.js
+++
@@ -1,8 +0,0 @@
-var initializeNewMember = function () {
-    return {
-        type: 'MEMBER_INITIALIZE_NEW'
-    };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = initializeNewMember;
-//# sourceMappingURL=initializeNewMember.js.map
