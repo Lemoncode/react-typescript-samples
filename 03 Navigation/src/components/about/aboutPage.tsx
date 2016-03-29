74196810aec7dd72ba8a5a992e0a20f231bf4f1d
nasdan <daniel.sanchez@lemoncode.net>
2016-03-29 20:38:28 +0200

    Added text to sample 03

--- 03 Navigation/src/components/about/aboutPage.tsx
+++ 03 Navigation/src/components/about/aboutPage.tsx
@@ -15,9 +15,25 @@ export default class About extends React.Component<Props, {}> {
              <div className="col-xs-12">
                <h1>
                  <small>
-                   Subtitle
+                   This sample takes as starting point sample "02 Components".
                  </small>
                </h1>
+               <div className="col-xs-12">
+                   <h3>
+                       <small>
+                           We are adding page navigation to this project:
+                       </small>
+                   </h3>
+                   <ul>
+                       <li><h3><small>We have added two pages (about, members).</small></h3></li>
+                       <li><h3><small>The user can navigate by clicking on links in a common navbar.</small></h3></li>
+                   </ul>
+                   <h3>
+                       <small>
+                           We are using <a target="_blank" href="https://github.com/reactjs/react-router">react-router</a> for the navigation support
+                       </small>
+                   </h3>
+               </div>
              </div>
 
              <div className="col-xs-12 top-buffer">
@@ -33,13 +49,11 @@ export default class About extends React.Component<Props, {}> {
              <div className="col-xs-12 top-buffer">
                <ul>
                 <li className="top-buffer">
-                 <h4><b>Index:</b></h4>
+                 <h4><b>Router:</b></h4>
                  <ul className="top-buffer">
                    <li>
                      <h4>
-                       httpReducer.ts: <small>Takes care of handling httpCallStarted and httpCallCompleted actions,
-                         updating the show spinner props (internally counts the number of requests that are in progres).
-                       </small>
+                       index.ts: <small>routes configuration.</small>
                      </h4>
                    </li>
                  </ul>
@@ -50,14 +64,17 @@ export default class About extends React.Component<Props, {}> {
                    <ul className="top-buffer">
                      <li>
                        <h4>
-                         MembersPage.tsx: <small>just fire two async request (Github json API), member and
-                           repositories belonging to a given organization.</small>
+                         app.tsx: <small>header + page container.</small>
                        </h4>
                      </li>
                      <li>
                        <h4>
-                         Spinner.tsx: <small>just fire two async request (Github json API), member and
-                           repositories belonging to a given organization.</small>
+                         header.tsx: <small>navigation links.</small>
+                       </h4>
+                     </li>
+                     <li>
+                       <h4>
+                         aboutPage.tsx / membersPage.tsx: <small>pages</small>
                        </h4>
                      </li>
                    </ul>
