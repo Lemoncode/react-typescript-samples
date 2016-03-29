9a7201c3e9f98ca54acc0737cd9eb150eee21a42
nasdan <daniel.sanchez@lemoncode.net>
2016-03-29 19:10:44 +0200

    Refactor AboutPage spec

--- 14 TestComponent/src/components/about/spec/aboutPage.spec.tsx
+++ 14 TestComponent/src/components/about/spec/aboutPage.spec.tsx
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import * as React from 'react';
 import { shallow } from 'enzyme';
+import { Link } from 'react-router';
 import AboutPage from '../aboutPage';
 
 describe('AboutPage presentational component', () =>{
@@ -15,17 +16,17 @@ describe('AboutPage presentational component', () =>{
     it('should renders a div with children h2 element with text equals "About Page"', () => {
         let aboutPageWrapper = shallow(<AboutPage />);
 
-        expect(aboutPageWrapper.find('h2')).to.have.length(1);
-        expect(aboutPageWrapper.find('h2').text()).to.be.equals("About Page");
+        expect(aboutPageWrapper.children().at(0).type()).to.be.equals('h2');
+        expect(aboutPageWrapper.children().at(0).text()).to.be.equals("About Page");
     });
 
     it('should renders a div with children Link element with text equals "Members" and property ' +
         'to equals "/members"', () => {
         let aboutPageWrapper = shallow(<AboutPage />);
 
-        expect(aboutPageWrapper.find('Link')).to.have.length(1);
+        expect(aboutPageWrapper.children().at(1).type()).to.be.equals(Link);
         //Need access to children because it's not a basic HTML Element
-        expect(aboutPageWrapper.find('Link').children().text()).to.be.equals("Members");
-        expect(aboutPageWrapper.find('Link').prop('to')).to.be.equals("/members");
+        expect(aboutPageWrapper.children().at(1).children().text()).to.be.equals("Members");
+        expect(aboutPageWrapper.children().at(1).prop('to')).to.be.equals("/members");
     });
 })
