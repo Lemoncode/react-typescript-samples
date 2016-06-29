a5949df3f09468c607729c1b914bfaafc1491ed6
nasdan <daniel.sanchez@lemoncode.net>
2016-06-29 15:14:15 +0200

    Fix sample 14

--- 14 TestComponent/typings-manual/redux-thunk/redux-thunk.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'redux-thunk' {
-    import { Middleware } from 'redux';
-    const thunkMiddleware : Middleware;
-    export default thunkMiddleware;
-}
--- 14 TestComponent/typings-manual/tsd.d.ts
+++ 14 TestComponent/typings-manual/tsd.d.ts
@@ -1,2 +1,1 @@
-/// <reference path="redux-thunk/redux-thunk.d.ts" />
 /// <reference path="redux-mock-store/redux-mock-store.d.ts" />
