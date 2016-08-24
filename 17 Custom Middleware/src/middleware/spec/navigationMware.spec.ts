98e055b03fa807ab3e5be63fb0397cca2b2db1f2
nasdan <daniel.sanchez@lemoncode.net>
2016-08-24 17:53:25 +0200

    import redux-mock-store

--- 17 Custom Middleware/src/middleware/spec/navigationMware.spec.ts
+++ 17 Custom Middleware/src/middleware/spec/navigationMware.spec.ts
@@ -1,7 +1,15 @@
 import { expect } from 'chai';
+import { navigationMiddleware } from '../navigationMware';
+import configureStore = require('redux-mock-store');
 
 describe('navigationMware', () => {
-  it('should return true if karma env is working', () =>{
+  it('should return true if karma env is working', () => {
+    //Arrange
+    const createStore = configureStore();
+
+    //Act
+
+    //Assert
     expect(true).to.be.true;
   })
 });
