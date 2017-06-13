704664fb20cd0c93e19dcd351b631b2bbf6e97eb
nasdan <daniel.sanchez@lemoncode.net>
2017-06-13 19:59:49 +0200

    Refactor components and add Readme.md

---
+++ 03 Navigation/src/components/members/page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const MembersPage: React.StatelessComponent<{}> = () => {
+  return (
+    <div className="row">
+      <h2> Members Page</h2>
+    </div>
+  );
+}
