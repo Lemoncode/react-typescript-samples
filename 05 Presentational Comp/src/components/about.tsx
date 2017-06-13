409374970abad7cc4c08693a09e349fcc5086aba
nasdan <daniel.sanchez@lemoncode.net>
2017-06-13 21:26:47 +0200

    Update About page

--- 05 Presentational Comp/src/components/about.tsx
+++ 05 Presentational Comp/src/components/about.tsx
@@ -3,24 +3,18 @@ import * as React from 'react';
 export const About: React.StatelessComponent<{}> = () => {
   return (
     <div className="row about-page">
-      <h1 className="jumbotron">04 DisplayData</h1>
+      <h1 className="jumbotron">05 Presentational Comp</h1>
 
       <div className="col-xs-12">
         <h1>
           <small>
-            This sample takes as starting point sample "03 Navigation".
+            This sample takes as starting point sample "04 DisplayData".
                  </small>
         </h1>
         <div className="col-xs-12">
           <h3>
-            <small>
-              We are adding a mock API to simulate fetching members data from Github:
-                       </small>
+            <small>We are extracting MemberHeader and MemberRow as Presentational Components</small>
           </h3>
-          <ul>
-            <li><h3><small>We have added memberAPI and mock data.</small></h3></li>
-            <li><h3><small>Update Members Page.</small></h3></li>
-          </ul>
         </div>
       </div>
 
@@ -28,47 +22,23 @@ export const About: React.StatelessComponent<{}> = () => {
         <h3>Highlights</h3>
         <hr />
         <h3>
-          <small>
-            The most interesting parts worth to take a look
-                 </small>
+          <small>The most interesting parts worth to take a look</small>
         </h3>
       </div>
 
       <div className="col-xs-12 top-buffer">
         <ul>
           <li className="top-buffer">
-            <h4><b>API:</b></h4>
-            <ul className="top-buffer">
-              <li>
-                <h4>
-                  api/member/index.ts: <small>API service.</small>
-                </h4>
-              </li>
-              <li>
-                <h4>
-                  api/member/mockData.ts: <small>Mock data.</small>
-                </h4>
-              </li>
-            </ul>
-          </li>
-
-          <li className="top-buffer">
-            <h4><b>Model:</b></h4>
-            <ul className="top-buffer">
-              <li>
-                <h4>
-                  model/memberEntity.ts: <small>Member model.</small>
-                </h4>
-              </li>
-            </ul>
-          </li>
-
-          <li className="top-buffer">
             <h4><b>Components:</b></h4>
             <ul className="top-buffer">
               <li>
                 <h4>
-                  members/page.tsx: <small>Members table</small>
+                  members/memberHeader.tsx: <small>Members table Header</small>
+                </h4>
+              </li>
+              <li>
+                <h4>
+                  members/memberRow.tsx: <small>Members table Row</small>
                 </h4>
               </li>
             </ul>
