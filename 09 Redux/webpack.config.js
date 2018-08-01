9a23f508c48f2f9f0071234e4b52b8669c5b5e89
Jehú <jehu.sagardoy@gmail.com>
2018-08-01 13:02:03 +0200

    refactored for new react version. Included browserRouter as Router

--- 09 Redux/webpack.config.js
+++ 09 Redux/webpack.config.js
@@ -1,6 +1,7 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let MiniCssExtractPlugin = require('mini-css-extract-plugin');
+let webpack = require('webpack');
 
 var basePath = __dirname;
 
@@ -15,7 +16,7 @@ module.exports = {
     vendor: [
       'react',
       'react-dom',
-      'react-router',
+      'react-router-dom',
       'toastr',
       'lc-form-validation',
       'redux',
@@ -25,7 +26,8 @@ module.exports = {
     vendorStyles: [
       '../node_modules/bootstrap/dist/css/bootstrap.css',
       '../node_modules/toastr/build/toastr.css',
-    ],
+    ]
+
   },
   output: {
     path: path.join(basePath, 'dist'),
@@ -34,7 +36,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts[x]?$/,
         exclude: /node_modules/,
         loader: 'awesome-typescript-loader',
         options: {
@@ -43,28 +45,14 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-        ],
-      },
-      // Loading glyphicons => https://github.com/gowravshekar/bootstrap-webpack
-      // Using here url-loader and file-loader
-      {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/img/[name].[ext]?[hash]'
+        }
       },
     ],
   },
@@ -81,8 +69,9 @@ module.exports = {
       template: 'index.html', //Name of template in ./src
       hash: true,
     }),
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['vendor', 'manifest'],
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     }),
   ],
 };
