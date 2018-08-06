dc543c1ab5bcf7f2d93f7f4c675e43d812db24c3
Jehú <jehu.sagardoy@gmail.com>
2018-08-06 13:39:38 +0200

    deleted because of newer version available

--- 10 SpinnerAsync/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './header';
-export * from './about';
-export * from './members';
-export * from './member';
--- 10 SpinnerAsync/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { AppRouter } from './router';
-
-ReactDOM.render(
-  <AppRouter />
-  , document.getElementById('root'));
