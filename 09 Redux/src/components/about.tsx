eca29cb8575ff2fbb0673e527226b2e63abed976
Jehú <jehu.sagardoy@gmail.com>
2018-08-08 14:27:02 +0200

    Fix for about page presentation

--- 09 Redux/src/components/about.tsx
+++ 09 Redux/src/components/about.tsx
@@ -2,21 +2,21 @@ import * as React from 'react';
 
 export const About: React.StatelessComponent<{}> = () => {
   return (
-    <div className="row about-page">
-      <h1 className="jumbotron">09 Redux</h1>
+    <div className="row about-page col-12">
+      <h1 className="jumbotron col-2">09 Redux</h1>
 
-      <div className="col-xs-12">
+      <div className="col-10" id="header-title">
         <h1>
           <small>This sample takes as starting point sample "08 ParamNavigation".</small>
         </h1>
-        <div className="col-xs-12">
+        <div className="col-10">
           <h3>
             <small>We are adding Redux pattern</small>
           </h3>
         </div>
       </div>
 
-      <div className="col-xs-12 top-buffer">
+      <div className="col-2 top-buffer">
         <h3>Highlights</h3>
         <hr />
         <h3>
@@ -24,7 +24,7 @@ export const About: React.StatelessComponent<{}> = () => {
         </h3>
       </div>
 
-      <div className="col-xs-12 top-buffer">
+      <div className="col-10">
         <ul>
           <li className="top-buffer">
             <h4><b>Actions:</b></h4>
