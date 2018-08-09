a6a495365be5262576c7418bbbbfe4fff61d7cae
Jehú <jehu.sagardoy@gmail.com>
2018-08-09 13:15:16 +0200

    updated packages and fixed about page

--- 05 Presentational Comp/src/components/about.tsx
+++ 05 Presentational Comp/src/components/about.tsx
@@ -2,23 +2,23 @@ import * as React from 'react';
 
 export const About: React.StatelessComponent<{}> = () => {
   return (
-    <div className="row about-page">
-      <h1 className="jumbotron">05 Presentational Comp</h1>
+    <div className="row about-page col-12">
+      <h1 className="jumbotron col-2">05 Presentational Comp</h1>
 
-      <div className="col-xs-12">
+      <div className="col-10" id="header-title" >
         <h1>
           <small>
             This sample takes as starting point sample "04 DisplayData".
                  </small>
         </h1>
-        <div className="col-xs-12">
+        <div className="col-10">
           <h3>
             <small>We are extracting MemberHeader and MemberRow as Presentational Components</small>
           </h3>
         </div>
       </div>
 
-      <div className="col-xs-12 top-buffer">
+      <div className="col-2 top-buffer">
         <h3>Highlights</h3>
         <hr />
         <h3>
@@ -26,7 +26,7 @@ export const About: React.StatelessComponent<{}> = () => {
         </h3>
       </div>
 
-      <div className="col-xs-12 top-buffer">
+      <div className="col-10">
         <ul>
           <li className="top-buffer">
             <h4><b>Components:</b></h4>
