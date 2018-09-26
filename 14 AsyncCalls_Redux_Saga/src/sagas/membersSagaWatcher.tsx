85019e147293400fa9c48c3cda5c6cd31d1713b5
Jehú <jehu.sagardoy@gmail.com>
2018-09-26 13:56:16 +0200

    included while true

--- 14 AsyncCalls_Redux_Saga/src/sagas/membersSagaWatcher.tsx
+++ 14 AsyncCalls_Redux_Saga/src/sagas/membersSagaWatcher.tsx
@@ -1,12 +1,15 @@
 
 import { fork, takeLatest, all, call } from 'redux-saga/effects';
 import { fetchMembers } from './membersSaga';
+import {actionTypes} from '../common/constants/actionTypes';
 
 // add here all your watchers
 // https://github.com/yelouafi/redux-saga/blob/master/examples/real-world/sagas/index.js
 function* watchLoadMembersRequest() {
         console.log("Estoy en members Saga llamando");
-        yield call(fetchMembers);
+        while (true){
+          yield* takeLatest (actionTypes.FETCH_MEMBERS_COMPLETED_SAGA,fetchMembers);
+        }
 }
 // Register all your watchers
 export default function* root() {
