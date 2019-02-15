9939a3b9bf265e2988737cbb3e99e84f9eedabc1
Braulio Diez Botella <braulio.diez@gmail.com>
2019-02-15 09:36:29 +0100

    03 state migrated

---
+++ hooks/03_State/src/app.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { HelloComponent } from "./hello";
+import { NameEditComponent } from "./nameEdit";
+
+export const App = () => {
+  const [name, setName] = React.useState("defaultUserName");
+
+  const setUsernameState = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <>
+      <HelloComponent userName={name} />
+      <NameEditComponent userName={name} onChange={setUsernameState} />
+    </>
+  );
+};
