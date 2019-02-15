25adbb7ca33c17194c4b816bcba5ff4c8c394aef
Braulio Diez Botella <braulio.diez@gmail.com>
2019-02-15 10:13:37 +0100

    04 callback migrated

---
+++ hooks/04_Callback/src/nameEdit.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+interface Props {
+  initialUserName: string;
+  onNameUpdated: (newName: string) => any;
+}
+
+export const NameEditComponent = (props: Props) => {
+  const [editingName, setEditingName] = React.useState(props.initialUserName);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditingName(e.target.value);
+  };
+
+  const onNameSubmit = (event: any): any => {
+    props.onNameUpdated(editingName);
+  };
+
+  return (
+    <>
+      <label>Update name:</label>
+      <input value={editingName} onChange={onChange} />
+      <button onClick={onNameSubmit}>Change</button>
+    </>
+  );
+};
