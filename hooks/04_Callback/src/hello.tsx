6bb418512827646cfb90d311fbd6afc71f63cf83
Uchan <shocking.white.0103@gmail.com>
2023-03-13 22:05:49 +0900

    init

--- hooks/04_Callback/src/hello.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-interface Props {
-  userName: string;
-}
-
-export const HelloComponent: React.FC<Props> = (props) => {
-  return <h2>Hello user: {props.userName} !</h2>;
-};
