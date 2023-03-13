fda92e3661c255f068e09ff142d214f7c16daa48
Uchan <shocking.white.0103@gmail.com>
2023-03-14 08:11:54 +0900

    init

--- hooks/06_Enable/src/app.tsx
+++ hooks/06_Enable/src/app.tsx
@@ -3,34 +3,26 @@ import { HelloComponent } from "./hello";
 import { NameEditComponent } from "./nameEdit";
 
 export const App = () => {
-  const [name, setName] = React.useState("defaultUserName");
-  const [editingName, setEditingName] = React.useState("defaultUserName");
+    const [name, setName] = React.useState('defaultUserName')
+    const [editingName, setEditingName] = React.useState("defaultUserName");
+    const setUsernameState = () => {
+        setName(editingName)
+    }
 
-  const loadUsername = () => {
-    setTimeout(() => {
-      setName("name from async call");
-      setEditingName("name from async call");
-    }, 500);
-  };
+    const loadUsername = () => {
+        setTimeout(() => {
+            setName("name from async call");
+            setEditingName("name from async call");
+        }, 500);
+    };
 
-  React.useEffect(() => {
-    loadUsername();
-  }, []);
-
-  const setUsernameState = () => {
-    setName(editingName);
-  };
-
-  return (
-    <>
-      <HelloComponent userName={name} />
-      <NameEditComponent
-        initialUserName={name}
-        editingName={editingName}
-        onNameUpdated={setUsernameState}
-        onEditingNameUpdated={setEditingName}
-        disabled={editingName === "" || editingName === name}
-      />
-    </>
-  );
+    React.useEffect(() => {
+        loadUsername()
+    }, []);
+    return (
+        <>
+            <HelloComponent userName={name} />
+            <NameEditComponent initialUserName={name} onNameUpdated={setUsernameState} editingName={editingName} onEditingNameUpdated={setEditingName} />
+        </>
+    );
 };
