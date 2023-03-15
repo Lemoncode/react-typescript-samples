7a590b9ff64caaed8398738ee04d49959cad9469
Uchan <shocking.white.0103@gmail.com>
2023-03-16 08:48:19 +0900

    colorpickerrefactor

--- hooks/08_ColorPickerRefactor/src/components/colorPicker.tsx
+++ hooks/08_ColorPickerRefactor/src/components/colorPicker.tsx
@@ -2,55 +2,46 @@ import * as React from "react";
 import { Color } from "../model/color";
 
 interface Props {
-  color: Color;
-  onColorUpdated: (color: Color) => void;
+    color: Color;
+    onColorUpdated: (color: Color) => void;
 }
-
-const updateColor = (props: Props, colorId: keyof Color) => (value: any) => {
-  // keyof Color ensures only 'red', 'blue' or 'green' can be passed in.
-  props.onColorUpdated({
-    ...props.color, // this creates a clone of the current props.color object...
-    [colorId]: value, // ... which gets one of its properties (colorId) immediately replaced by a new value.
-  });
-};
-
-export const ColorPicker: React.FC<Props> = (props) => (
-  <div>
-    <ColorSliderComponent
-      value={props.color.red}
-      onValueUpdated={updateColor(props, "red")}
-    />
-    <br />
-    <ColorSliderComponent
-      value={props.color.green}
-      onValueUpdated={updateColor(props, "green")}
-    />
-    <br />
-    <ColorSliderComponent
-      value={props.color.blue}
-      onValueUpdated={updateColor(props, "blue")}
-    />
-    {props.color.blue}
-    <br />
-  </div>
-);
 
 interface PropsColorSlider {
-  value: number;
-  onValueUpdated: (newValue: number) => void;
+    key: string;
+    value: number;
+    onValueUpdated: (newValue: number) => void;
 }
 
-const ColorSliderComponent: React.FC<PropsColorSlider> = (props: PropsColorSlider) => {
-  return (
-    <div>
-      <input
-        type="range"
-        min="0"
-        max="255"
-        value={props.value}
-        onChange={(event) => props.onValueUpdated(+event.target.value)}
-      />
-      {props.value}
-    </div>
-  );
-};
+const ColorSliderComponent = (props: PropsColorSlider) => {
+    return (
+        <div>
+            <input
+                type="range"
+                min="0"
+                max="255"
+                value={props.value}
+                onChange={event => props.onValueUpdated(+event.target.value)}
+            />
+            {props.value}
+        </div>
+    )
+}
+
+const updateColor = (props: Props, colorId: keyof Color) => (value) => {
+    props.onColorUpdated({
+        ...props.color,
+        [colorId]: value
+    })
+}
+
+export const ColorPicker = (props: Props) => (
+    <>
+        {Object.keys(props.color).map((field: keyof Color) => (
+            <ColorSliderComponent
+                key={field}
+                value={props.color[field]}
+                onValueUpdated={updateColor(props, field)}
+            />
+        ))}
+    </>
+);
--- hooks/08_ColorPickerRefactor/webpack.config.js
+++ hooks/08_ColorPickerRefactor/webpack.config.js
@@ -1,63 +1,60 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require("webpack");
-const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var webpack = require("webpack");
+var path = require("path");
 
-const basePath = __dirname;
+var basePath = __dirname;
 
 module.exports = {
   context: path.join(basePath, "src"),
   resolve: {
-    extensions: [".js", ".ts", ".tsx"],
+    extensions: [".js", ".ts", ".tsx"]
   },
-  entry: ["@babel/polyfill", "./index.tsx"],
+  entry: ["@babel/polyfill", "./components/index.tsx"],
   output: {
     path: path.join(basePath, "dist"),
-    filename: "bundle.js",
+    filename: "bundle.js"
   },
   devtool: "source-map",
   devServer: {
-    contentBase: "./dist", // Content base
-    inline: true, // Enable watch and live reload
-    host: "localhost",
-    port: 8080,
-    stats: "errors-only",
+    // inline: true, // Enable watch and live reload
+    // host: "localhost",
+    // port: 8080,
+    // stats: "errors-only",
+    static: {
+      directory: "./dist",
+    }
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        loader: "awesome-typescript-loader",
-        options: {
-          useBabel: true,
-          babelCore: "@babel/core", // needed for Babel v7
-        },
+        loader: "ts-loader",
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: "file-loader",
         options: {
-          name: "assets/img/[name].[ext]?[hash]",
-          esModule: false,
-        },
-      },
-    ],
+          name: "assets/img/[name].[ext]?[hash]"
+        }
+      }
+    ]
   },
   plugins: [
     //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: "index.html", //Name of file in ./dist/
       template: "index.html", //Name of template in ./src
-      hash: true,
+      hash: true
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-  ],
+      chunkFilename: "[id].css"
+    })
+  ]
 };
