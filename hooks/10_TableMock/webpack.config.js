5e80896f2feaec1920d0520131f3ddc2783cfa2b
Uchan <shocking.white.0103@gmail.com>
2023-03-18 11:58:19 +0900

    tablemock

--- hooks/10_TableMock/webpack.config.js
+++ hooks/10_TableMock/webpack.config.js
@@ -1,80 +1,78 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require("webpack");
-const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var webpack = require("webpack");
+var path = require("path");
 
-const basePath = __dirname;
+var basePath = __dirname;
 
 module.exports = {
   context: path.join(basePath, "src"),
   resolve: {
-    extensions: [".js", ".ts", ".tsx", ".css"],
+    extensions: [".js", ".ts", ".tsx", ".css"]
   },
-  entry: ["@babel/polyfill", "./index.tsx"],
+  entry: ["@babel/polyfill", "./components/index.tsx"],
   output: {
     path: path.join(basePath, "dist"),
-    filename: "bundle.js",
+    filename: "bundle.js"
   },
   devtool: "source-map",
   devServer: {
-    contentBase: "./dist", // Content base
-    inline: true, // Enable watch and live reload
-    host: "localhost",
-    port: 8080,
-    stats: "errors-only",
+    // inline: true, // Enable watch and live reload
+    // host: "localhost",
+    // port: 8080,
+    // stats: "errors-only",
+    static: {
+      directory: "./dist",
+    }
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        loader: "awesome-typescript-loader",
-        options: {
-          useBabel: true,
-          babelCore: "@babel/core", // needed for Babel v7
-        },
+        loader: "ts-loader",
       },
       {
         test: /\.css$/,
         include: /node_modules/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
+      // Use CSS modules for custom stylesheets
       {
         test: /\.css$/,
         exclude: /node_modules/,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
+              modules: true,
               modules: {
                 localIdentName: "[name]__[local]___[hash:base64:5]",
               },
-              localsConvention: "camelCase",
             },
-          },
-        ],
+          }
+        ]
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: "file-loader",
         options: {
-          name: "assets/img/[name].[ext]?[hash]",
-          esModule: false,
-        },
-      },
-    ],
+          name: "assets/img/[name].[ext]?[hash]"
+        }
+      }
+    ]
   },
   plugins: [
     //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: "index.html", //Name of file in ./dist/
       template: "index.html", //Name of template in ./src
-      hash: true,
+      hash: true
     }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-  ],
+      chunkFilename: "[id].css"
+    })
+  ]
 };
