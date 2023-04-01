a65782f02204342b48975a037f3f2e0191dc8610
Uchan <shocking.white.0103@gmail.com>
2023-04-01 20:59:22 +0900

    context

--- hooks/15_Context/src/app.tsx
+++ hooks/15_Context/src/app.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { SessionProvider } from "./common/sessionContext";
 import { LoginComponent } from "./pages/login.component";
 import { LoginContainer } from "./pages/login.container";
 import { PageB } from "./pages/pageB";
@@ -13,6 +14,7 @@ export const App = () => {
         <>
             <BrowserRouter>
                 <Routes>
+                    {/* <Route path="/" element={<SessionProvider />} /> */}
                     <Route path="/" element={<LoginContainer />} />
                     <Route path="/pageB" element={<PageB />} />
                 </Routes>
--- hooks/15_Context/src/common/index.ts
+++ hooks/15_Context/src/common/index.ts
@@ -1,2 +1,3 @@
 export * from "./notification";
 export * from "./textField.component";
+// export * from "./sessionContext";
